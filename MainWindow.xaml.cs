e2b1239244e77ffa0561fbc50c4eb11bf497ac7d
Tector <73559984+Tectors@users.noreply.github.com>
2024-12-26 21:00:37 -0800

    fix toolbar

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,12 @@
-﻿using System.Text;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Windows.Shell;
-using Window = System.Windows.Window;
+﻿using AdonisUI.Controls;
 
-namespace LocalFetch;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow
+namespace LocalFetch
 {
-    public MainWindow()
+    public partial class MainWindow : AdonisWindow
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
     }
 }
