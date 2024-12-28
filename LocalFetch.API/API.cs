3367892d0ba03c1c246900ec8b7f41c3d34fd689
Tector <73559984+Tectors@users.noreply.github.com>
2024-12-28 15:40:27 -0800

    changes

--- LocalFetch.API/API.cs
+++ LocalFetch.API/API.cs
@@ -1,78 +1,77 @@
 using CUE4Parse.FileProvider.Vfs;
 using Microsoft.EntityFrameworkCore;
-using LocalFetchRestAPI.Controllers;
+using LocalFetch.API.Controllers;
 
 // Startup of Local Fetch's API
 // The API is at http://localhost:1500
 //
 // If you know any better, remove the Main function from this, as it's not needed, but I'm not quite sure how to without compile errors
 
-namespace LocalFetchRestAPI
+namespace LocalFetch.API;
+
+public class LocalFetchApi
 {
-    public class LocalFetchApi
+    public static AbstractVfsFileProvider? Provider;
+
+    public LocalFetchApi(AbstractVfsFileProvider provider)
     {
-        public static AbstractVfsFileProvider? Provider;
+        Provider = provider;
+    }
 
-        public LocalFetchApi(AbstractVfsFileProvider provider)
+    public async Task RunApi(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var services = builder.Services;
+
+        builder.Logging.ClearProviders();
+        builder.Logging.AddConsole();
+        builder.Logging.SetMinimumLevel(LogLevel.Information);
+        builder.Logging.AddFilter("Microsoft.Hosting.Lifetime", LogLevel.None);
+        builder.Services.AddControllers().AddApplicationPart(typeof(LocalFetchApiController).Assembly);
+
+        services.AddDbContext<DbContext>(opt => opt.UseInMemoryDatabase("LocalFetchRestAPI"));
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+
+        services.AddHsts(options =>
         {
-            Provider = provider;
+            options.Preload = true;
+            options.IncludeSubDomains = true;
+            options.MaxAge = TimeSpan.FromDays(365);
+        });
+
+        services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(builder =>
+            {
+                builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+            });
+        });
+
+        var app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
         }
 
-        public async Task RunApi(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            var services = builder.Services;
+        app.UseCors();
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapControllers();
 
-            builder.Logging.ClearProviders();
-            builder.Logging.AddConsole();
-            builder.Logging.SetMinimumLevel(LogLevel.Information);
-            builder.Logging.AddFilter("Microsoft.Hosting.Lifetime", LogLevel.None);
-            builder.Services.AddControllers().AddApplicationPart(typeof(LocalFetchApiController).Assembly);
+        Console.WriteLine("Web API is running in the background.");
 
-            services.AddDbContext<DbContext>(opt => opt.UseInMemoryDatabase("LocalFetchRestAPI"));
-            services.AddControllers();
-            services.AddEndpointsApiExplorer();
-
-            services.AddHsts(options =>
-            {
-                options.Preload = true;
-                options.IncludeSubDomains = true;
-                options.MaxAge = TimeSpan.FromDays(365);
-            });
-
-            services.AddCors(options =>
-            {
-                options.AddDefaultPolicy(builder =>
-                {
-                    builder.AllowAnyOrigin()
-                           .AllowAnyMethod()
-                           .AllowAnyHeader();
-                });
-            });
-
-            var app = builder.Build();
-
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
-            }
-
-            app.UseCors();
-            app.UseHttpsRedirection();
-            app.UseRouting();
-            app.UseAuthentication();
-            app.UseAuthorization();
-            app.MapControllers();
-
-            Console.WriteLine("Web API is running in the background.");
-
-            await app.RunAsync("http://localhost:1500");
-        }
+        await app.RunAsync("http://localhost:1500");
+    }
         
-        // Doesn't compile without this ?
-        public static void Main(string[] args)
-        {
-        }
+    // Doesn't compile without this ?
+    public static void Main(string[] args)
+    {
     }
 }
--- LocalFetch.API/Controllers/Controller.cs
+++ LocalFetch.API/Controllers/Controller.cs
@@ -16,139 +16,138 @@ using SkiaSharp;
 // _provider is from the Local Fetch Web Application, as we don't want to start two different CUE4Parses
 // --------------------------------------------------------------------------------------------------------------------------------
 
-namespace LocalFetchRestAPI.Controllers
+namespace LocalFetch.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LocalFetchApiController(DbContext context) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class LocalFetchApiController(DbContext context) : ControllerBase
-    {
-        // -----------------------------------------------------------------------------------------------------------------------
-        private readonly DbContext _context = context;
-        private readonly AbstractVfsFileProvider? _provider = LocalFetchApi.Provider; // Set from LocalFetch
-        // -----------------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------------
+    private readonly DbContext _context = context;
+    private readonly AbstractVfsFileProvider? _provider = LocalFetchApi.Provider; // Set from LocalFetch
+    // -----------------------------------------------------------------------------------------------------------------------
         
-        // Normal Export
-        [HttpGet("/api/v1/export")]
-        public ActionResult Get(bool raw, string path)
+    // Normal Export
+    [HttpGet("/api/v1/export")]
+    public ActionResult Get(bool raw, string path)
+    {
+        var contentType = Request.Headers.ContentType;
+        path = path.SubstringBefore('.');
+
+        try
         {
-            var contentType = Request.Headers.ContentType;
-            path = path.SubstringBefore('.');
+            var localObject = _provider?.LoadObject(path);
 
-            try
-            {
-                var localObject = _provider?.LoadObject(path);
-
-                // Return a raw export
-                if (raw) return HandleRawExport(path);
+            // Return a raw export
+            if (raw) return HandleRawExport(path);
                 
-                // Switch on Class Type
-                switch(localObject)
-                {
-                    case UTexture texture:
-                        return ProcessTexture(texture, contentType);
-                    case USoundWave wave:
-                        return ProcessSoundWave(wave);
-                };
-
-                return HandleRawExport(path);
-            }
-            catch (Exception exception)
+            // Switch on Class Type
+            switch(localObject)
             {
-                return Conflict(new
-                {
-                    errored = true,
-                    note = exception.Message.StartsWith("One or more errors occurred. (There is no game file with the path ")
-                        ? "Unable to find package"
-                        : exception.Message
-                });
-            }
-        }
-
-        // Return a texture as a file / encoding
-        private ActionResult ProcessTexture(UTexture texture, string contentType)
-        {
-            if (texture.GetFirstMip()?.BulkData.Data is { } mipData && contentType == "application/octet-stream")
-            {
-                return File(mipData, contentType);
-            }
-
-            SKBitmap? textureData = texture.Decode();
-            if (textureData == null)
-            {
-                return StatusCode(500, new
-                {
-                    errored = true,
-                    exceptionstring = "Invalid texture data, exported as json",
-                    jsonOutput = new { texture }
-                });
-            }
-
-            return File(textureData.Encode(SKEncodedImageFormat.Png, quality: 100).AsStream(), contentType);
-        }
-
-        // Return a sound wave
-        private ActionResult ProcessSoundWave(USoundWave wave)
-        {
-            wave.Decode(true, out var audioFormat, out var data);
-            if (data == null || string.IsNullOrEmpty(audioFormat))
-            {
-                return Conflict(new
-                {
-                    errored = true,
-                    exceptionstring = "Invalid audio data, exported as json",
-                    jsonOutput = new[] { wave }
-                });
-            }
-
-            var mimeType = audioFormat.ToLower() switch
-            {
-                "wem" => "application/vnd.wwise.wem",
-                "wav" => "audio/wav",
-                "adpcm" => "audio/adpcm",
-                "opus" => "audio/opus",
-                _ => "audio/ogg"
+                case UTexture texture:
+                    return ProcessTexture(texture, contentType);
+                case USoundWave wave:
+                    return ProcessSoundWave(wave);
             };
 
-            return File(data, mimeType);
+            return HandleRawExport(path);
+        }
+        catch (Exception exception)
+        {
+            return Conflict(new
+            {
+                errored = true,
+                note = exception.Message.StartsWith("One or more errors occurred. (There is no game file with the path ")
+                    ? "Unable to find package"
+                    : exception.Message
+            });
+        }
+    }
+
+    // Return a texture as a file / encoding
+    private ActionResult ProcessTexture(UTexture texture, string contentType)
+    {
+        if (texture.GetFirstMip()?.BulkData.Data is { } mipData && contentType == "application/octet-stream")
+        {
+            return File(mipData, contentType);
         }
 
-        // Handle raw exports
-        private ActionResult HandleRawExport(string path)
+        SKBitmap? textureData = texture.Decode();
+        if (textureData == null)
         {
-            var objectPath = path.SubstringBefore('.') + ".o.uasset";
-            var exports = _provider?.LoadAllObjects(path);
-            var finalExports = new List<UObject>(exports);
-
-            finalExports.AddRange(exports);
-
-            var mergedExports = new List<UObject>();
-            if (_provider != null && _provider.TryLoadPackage(objectPath, out var editorAsset))
+            return StatusCode(500, new
             {
-                foreach (var export in exports)
-                {
-                    var editorData = editorAsset.GetExportOrNull(export.Name + "EditorOnlyData");
-                    if (editorData != null)
-                    {
-                        export.Properties.AddRange(editorData.Properties);
-                        mergedExports.Add(export);
-                    }
-                }
+                errored = true,
+                exceptionstring = "Invalid texture data, exported as json",
+                jsonOutput = new { texture }
+            });
+        }
 
-                foreach (var editorExport in editorAsset.GetExports())
+        return File(textureData.Encode(SKEncodedImageFormat.Png, quality: 100).AsStream(), contentType);
+    }
+
+    // Return a sound wave
+    private ActionResult ProcessSoundWave(USoundWave wave)
+    {
+        wave.Decode(true, out var audioFormat, out var data);
+        if (data == null || string.IsNullOrEmpty(audioFormat))
+        {
+            return Conflict(new
+            {
+                errored = true,
+                exceptionstring = "Invalid audio data, exported as json",
+                jsonOutput = new[] { wave }
+            });
+        }
+
+        var mimeType = audioFormat.ToLower() switch
+        {
+            "wem" => "application/vnd.wwise.wem",
+            "wav" => "audio/wav",
+            "adpcm" => "audio/adpcm",
+            "opus" => "audio/opus",
+            _ => "audio/ogg"
+        };
+
+        return File(data, mimeType);
+    }
+
+    // Handle raw exports
+    private ActionResult HandleRawExport(string path)
+    {
+        var objectPath = path.SubstringBefore('.') + ".o.uasset";
+        var exports = _provider?.LoadAllObjects(path);
+        var finalExports = new List<UObject>(exports);
+
+        finalExports.AddRange(exports);
+
+        var mergedExports = new List<UObject>();
+        if (_provider != null && _provider.TryLoadPackage(objectPath, out var editorAsset))
+        {
+            foreach (var export in exports)
+            {
+                var editorData = editorAsset.GetExportOrNull(export.Name + "EditorOnlyData");
+                if (editorData != null)
                 {
-                    if (!mergedExports.Contains(editorExport))
-                    {
-                        finalExports.Add(editorExport);
-                    }
+                    export.Properties.AddRange(editorData.Properties);
+                    mergedExports.Add(export);
                 }
             }
-            mergedExports.Clear();
 
-            // Serialize object, and return it indented
-            return new OkObjectResult(JsonConvert.SerializeObject(new
+            foreach (var editorExport in editorAsset.GetExports())
             {
-                jsonOutput = finalExports
-            }, Formatting.Indented));
+                if (!mergedExports.Contains(editorExport))
+                {
+                    finalExports.Add(editorExport);
+                }
+            }
         }
+        mergedExports.Clear();
+
+        // Serialize object, and return it indented
+        return new OkObjectResult(JsonConvert.SerializeObject(new
+        {
+            jsonOutput = finalExports
+        }, Formatting.Indented));
     }
 }
