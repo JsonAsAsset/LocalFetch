bcc374fb4986677bdc43f3f49e090f35490fc3a7
Tector <73559984+Tectors@users.noreply.github.com>
2025-02-28 07:59:54 -0800

    more work

--- LocalFetch/Views/NewLocalFetchViewModel.cs
+++ LocalFetch/Views/NewLocalFetchViewModel.cs
@@ -1,12 +1,18 @@
+using System;
+using System.Collections.Generic;
 using Avalonia.Controls;
+using CUE4Parse.UE4.Versions;
 
 namespace LocalFetch.Views
 {
     public partial class NewLocalFetchView : Window
     {
+        public List<EGame> Items { get; } = new List<EGame>((Enum.GetValues(typeof(EGame)) as EGame[])!);
+
         public NewLocalFetchView()
         {
             InitializeComponent();
+            DataContext = this;
         }
 
         private void CreateButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
